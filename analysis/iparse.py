13ec10edc3ab42a018a012dadcc871763d36b882
Thomas Lu <thomas@Thomass-MacBook-Air.local>
2021-04-08 01:09:15 -0700

    fix to iparse

--- analysis/iparse.py
+++ analysis/iparse.py
@@ -41,9 +41,10 @@ class IParser():
                            _pos=[(w, 'UNK') for w in words])
         ])
         tree, cats = self.parser.parse(treebank, return_cats=True, tau=0)[0]
-        w, d = cats.shape
-        cats = cats.reshape(w, self.parser.tags_per_word,
-                            d // self.parser.tags_per_word)
+        if self.parser.tags_per_word > 1:
+            w, d = cats.shape
+            cats = cats.reshape(w, self.parser.tags_per_word,
+                                d // self.parser.tags_per_word)
         cats = cats.argmax(-1).cpu().numpy().tolist()
         self.remove_pos(tree)
         for i in range(len(tree.leaves())):
